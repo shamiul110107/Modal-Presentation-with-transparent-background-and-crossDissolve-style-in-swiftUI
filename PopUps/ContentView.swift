a21ef7a0172176652a17754f8ee12476f9bf4985
Md. Shamiul Islam <shamiul.islam@bs-23.net>
2020-02-25 14:06:06 +0600

    Initial Commit

---
+++ PopUps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopUps
+//
+//  Created by sami on 25/2/20.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
